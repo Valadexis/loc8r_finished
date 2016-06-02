8b710113e6d42f7f544a1d9cacc0c6b76586df62
Steve D <Steve D>
2016-06-02 10:47:50 -0700

    number5

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -34,8 +34,8 @@ module.exports.locationsListByDistance = function(req, res) {
   };
   var geoOptions = {
     spherical: true,
-    maxDistance: theEarth.getRadsFromDistance(maxDistance),
-    num: 50000
+    maxDistance: 50000,
+    num: 10
   };
   if ((!lng && lng!==0) || (!lat && lat!==0) || ! maxDistance) {
     console.log('locationsListByDistance missing params');
